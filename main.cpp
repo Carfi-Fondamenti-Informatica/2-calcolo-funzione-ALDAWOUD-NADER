651ceba0b306550a6e50815605bd30a44a3c4cb5
ALDAWOUD-NADER <100731947+ALDAWOUD-NADER@users.noreply.github.com>
2022-04-01 15:55:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
-
+float a=0, b=0, x=0, y=0, z=0;
+    cout << "inserire i valori di a, b, x, y" << endl;
+    cin >> a >> b >> x >> y;
+    if (x<0 && y>0){
+        z = (a*x) - (b*y);
+    }
+    else if (x>=0 && y<=0){
+        z=(a*(x*x)) - (b*y);
+    }
+    else {
+        z=(a*x) + (b*(y*y));
+    }
+    cout << z;
+   return 0;
 int main() {
    return 0;
 }
